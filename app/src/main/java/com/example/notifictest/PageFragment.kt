e36dc66ee96bd330ebe44dbb19a5cfbe4398a7f0
Volodymyr Andrukhiv <vovaandrukhiv@gmail.com>
2021-08-29 17:47:56 +0300

    Допрацьовано завдання

--- app/src/main/java/com/example/notifictest/PageFragment.kt
+++ app/src/main/java/com/example/notifictest/PageFragment.kt
@@ -3,20 +3,64 @@ package com.example.notifictest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
 import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+const val KEY_ITEM_TEXT = "com.example.notifictest.KEY_ITEM_TEXT"
+
+class PageFragment : Fragment() {
+
+    private val TAG = "PageFragment"
+    private var index = ""
 
 
-class MNotification(private val context: Context?) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view = inflater.inflate(R.layout.item_mutable_collection, container, false)
+
+        val buttonNotification: Button = view.findViewById(R.id.buttonNotification)
+        buttonNotification.setText(R.string.create_new_notification)
+        index = arguments?.getString(KEY_ITEM_TEXT) ?: throw IllegalStateException()
+
+        buttonNotification.setOnClickListener {
+
+//            val toast = Toast.makeText(
+//                activity,
+//                "Notification $index", Toast.LENGTH_SHORT
+//            )
+//            toast.show()
+
+            Log.i(TAG, "Сповіщення $index")
+
+            notificationReminder()
+        }
+        return view
+    }
 
     companion object {
         const val NOTIFICATION_ID = 101
         const val CHANNEL_ID = "channelID"
+
+        fun newInstance(itemText: String) =
+            PageFragment().apply {
+                arguments = Bundle(1).apply {
+                    putString(KEY_ITEM_TEXT, itemText)
+                }
+            }
     }
 
     private val largeIcon = BitmapFactory.decodeResource(
@@ -32,7 +76,7 @@ class MNotification(private val context: Context?) {
 //        val pendingIntent =
 //            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 //
-//        val openIntent = Intent(context, MNotification::class.java)
+//        val openIntent = Intent(context, PageFragment::class.java)
 //        val pendingOpenIntent =
 //        PendingIntent.getActivity(context, 1, openIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
@@ -45,12 +89,13 @@ class MNotification(private val context: Context?) {
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.ic_messenger)
                     .setContentTitle("Chat heads active")
-                    .setContentText("Notification " + PageFragment.index)
-                    //.addAction(0, "Action", pendingOpenIntent)
-                    .setColor(ContextCompat.getColor(context, R.color.messenger))
+                    .setContentText("Notification $index")
+                    .setAutoCancel(true)
+                 // .addAction(0, "Action", pendingOpenIntent)
+                    .setColor(ContextCompat.getColor(requireContext(), R.color.messenger))
                     .setStyle(NotificationCompat.BigTextStyle())// Стиль сповіщення
                     .setSound(defaultSoundUri) // Встановити звук
-                    // .setContentIntent(pendingIntent)
+                 // .setContentIntent(pendingIntent)
             }
 
         // Надіслати сповіщення
